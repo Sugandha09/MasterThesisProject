49b7f27d16d699d1b3c77b5707871ed5ba73cc5e
Sugandha09 <100796127+Sugandha09@users.noreply.github.com>
2022-05-30 07:09:50 +0100

    Update eda.py

--- pages/eda.py
+++ pages/eda.py
@@ -13,10 +13,8 @@ def app():
     #advert_report = sv.analyze(data)
     #display the report
     #advert_report.show_html('edatemp.html')
-    eda1 = open("edatemp.html", 'r', encoding='utf-8')
-    x = eda1.read() 
-    print(x)
-    components.html(x, height=800, width=1000)
+    st.write('Click the following EDA link of City Tempersture dataset')
+    st.write('https://mi-linux.wlv.ac.uk/~2048496/edatemp.html')
     
     #data1=pd.read_csv('westmidlands_crime.csv')
     #data1=data1.drop('Crime ID', axis=1)
@@ -25,19 +23,14 @@ def app():
     #display the report
     #advert_report1.show_html('edacrime.html')
     #components.iframe(src='edacrime.html', width=1100, height=1200, scrolling=True)
-    eda2 = open("edacrime.html", 'r', encoding='utf-8')
-    y = eda2.read() 
-    print(y)
-    components.html(y, height=800, width=1000)
-   
+    st.write('Click the following EDA link of Westmidlands Crime dataset')
+    st.write('https://mi-linux.wlv.ac.uk/~2048496/edacrime.html')
+    
     #data2=pd.read_csv('melanoma-1.csv')
     #data2['year']=data2['year'].astype('category')
     #advert_report2 = sv.analyze(data2)
     #display the report
     #advert_report2.show_html('edamelanoma.html')
     #components.iframe(src='edamelanoma.html', width=1100, height=1200, scrolling=True)
-    eda3 = open("edamelanoma.html", 'r', encoding='utf-8')
-    z = eda3.read() 
-    print(z)
-    components.html(z, height=800, width=1000)
-   
+    st.write('Click the following EDA link of Malignant Melanoma dataset')
+    st.write('https://mi-linux.wlv.ac.uk/~2048496/edamelanoma.html')
